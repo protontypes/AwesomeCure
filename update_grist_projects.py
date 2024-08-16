e3870f511c27ea4cb05db61da6570a71bbe544da
Tobias Augspurger <ly0@protonmail.com>
2024-08-16 12:40:01 +0200

    simple ecosyste.ms integration.

--- update_grist_projects.py
+++ update_grist_projects.py
@@ -1,18 +1,100 @@
 import json
-import os
 import requests
 import pandas as pd
 import math
 from os import getenv
 from dotenv import load_dotenv
 
+
+from habanero import counts
+
+from semanticscholar import SemanticScholar
+sch = SemanticScholar()
+
+URL = "https://ost.ecosyste.ms/api/v1/projects?reviewed=true&per_page=3000"
+FILE_TO_SAVE_AS = "ecosystems_repository_downloads.json" # the name you want to save file as
+
+pages=20
+resp = requests.get(URL) # making requests to server
+
+#all_data = {}
+#for page in range(1, pages + 1):
+#    data = requests.get(URL.format(page)).json()
+#    all_data.extend(data)
+    
+    
+#for page in range(1, pages + 1):
+#    print("Page:",page)
+#    data = requests.get(URL.format(page)).json()
+#    if 'status' in data:
+#        break
+#    all_data = {**all_data, **data[0]}
+
+with open(FILE_TO_SAVE_AS, "wb") as f: # opening a file handler to create new file 
+    f.write(resp.content) # writing content to file
+df_ecosystems = pd.read_json(resp.content.decode())
+
+
+from urllib.parse import urlparse
+names = []
+download_counts = []
+url = []
+description = []
+category = []
+sub_category = []
+language = []
+docker_downloads = []
+DOIs = []
+total_citations = []
+
+for index, row in df_ecosystems.iterrows():
+    names.append(row['name'])
+    package_downloads = 0
+    docker_download_count = 0
+    for package_manager in range(len(row['packages'])):
+        if row['packages'][package_manager]['downloads']:
+            if row['packages'][package_manager]['downloads_period'] == "last-month":
+                package_downloads += row['packages'][package_manager]['downloads']
+        if row['packages'][package_manager]['docker_downloads_count']:
+            docker_download_count = row['packages'][package_manager]['docker_downloads_count']
+        else:
+            docker_download_count = 0
+    download_counts.append(package_downloads)
+    docker_downloads.append(docker_download_count)
+    url.append(row['url'])
+    description.append(row['description'])
+    category.append(row['category'])
+    sub_category.append(row['sub_category'])
+    language.append(row['language'])
+    total_citations.append(row['total_citations']) 
+    if row['readme_doi_urls']:
+        doi = urlparse(row['readme_doi_urls'][0]).path[1:]
+        DOIs.append(doi)
+    else:
+        DOIs.append(None)
+
+
+df_extract = pd.DataFrame()
+df_extract['project_names'] = names
+df_extract['download_counts'] = download_counts
+df_extract['citations'] = total_citations
+df_extract['doi'] = DOIs
+df_extract['docker_downloads'] = docker_downloads
+#df_extract['category'] = category
+df_extract['sub_category'] = sub_category
+df_extract['git_url'] = url
+df_extract['description'] = description
+df_extract['language'] = language
+df_extract['docker_downloads'] = docker_downloads
+
+
 assert load_dotenv(), 'Environment variables could not be loaded'
 
 # Replace these with your values
 API_KEY = getenv("GRIST")
-DOC_ID = 'gSscJkc5Rb1Rw45gh1o1Yc'
+DOC_ID = '8YWKLVW6EKD7sLxWP2H9ZY'
 TABLE_NAME = 'Projects'
-CSV_FILE_PATH = './csv/projects.csv'
+CSV_FILE_PATH = './csv/ost_deployment.csv'
 MAX_BYTES = 500_000
 
 # Headers for API request
@@ -75,47 +157,90 @@ def handle_response(response):
         except (ValueError, KeyError):
             raise e
 
+
+# Function to create a new column in the table
+def create_column(column_name, column_type='Text'):
+    # Define the API endpoint for adding a column
+    url = f'{BASE_URL}/tables/{TABLE_ID}/columns'
+    
+    # Data to be sent in the request body
+    data = {
+        'fields': {
+            'id': column_name.replace(" ", "_").lower(),  # Convert to snake_case for ID
+            'label': column_name,                        # The column name as it appears in the UI
+            'type': column_type,                         # The type of the column ('Text', 'Numeric', etc.)
+        }
+    }
+    
+    # Make the POST request to create the column
+    response = requests.post(url, json=data, headers=headers)
+    
+    # Check if the request was successful
+    if response.status_code == 200:
+        print(f"Column '{column_name}' created successfully!")
+    else:
+        print(f"Failed to create column '{column_name}'. Status Code: {response.status_code}")
+
+
 # Load and clean data
-df = pd.read_csv(CSV_FILE_PATH)  # Load data from CSV file
-df = df.where(pd.notna(df), None)  # Replace NaN values with None
+df = df_extract # Load data from CSV file
+df = df.where(pd.notna(df_extract), None)  # Replace NaN values with None
+
+column_names = list(df.columns.values)
+
+
+column_types = {
+    'project_names': 'Text',
+    'download_counts': 'Numeric',
+    'citations': 'Integer',
+    'doi': 'Text',
+    'docker_downloads': 'Integer',
+    'sub_category': 'Text',
+    'git_url': 'Text',
+    'description': 'Text',
+    'language': 'Text'
+}
+
+columns_to_create = [
+    {
+        'id': column_name.replace(" ", "_").lower(),  
+        'label': column_name,                        
+    }
+    for column_name in column_names
+]
+
+print("Columns defined:",column_names)
 
 with requests.Session() as session:  # Using requests.Session for multiple requests
     session.headers.update(headers)  # Update session headers
 
     # Fetch column data from API
     columns_url = f'https://api.getgrist.com/api/docs/{DOC_ID}/tables/{TABLE_NAME}/columns'
-    response = handle_response(session.get(columns_url))  # Handle response
+    
+    response = handle_response(session.post(columns_url, json={'columns': columns_to_create}))
+    if response.status_code != 200:
+        print("Failed to create columns existing records")
+        print(response.json())
+        exit()
 
-    # Create a mapping from label to colRef (column ID)
-    columns_data = response.json()
-    column_mapping = {col["fields"]["label"]: col["id"] for col in columns_data["columns"]}
-
-    # Check if all columns in dataframe exist in Grist table
-    for col in df.columns:
-        if col not in column_mapping.keys():
-            print(f"Column '{col}' does not exist in Grist table. Removing it from dataframe.")
-            df = df.drop(columns=[col])  # Remove non-existent columns
-
-    # Rename columns
-    df = df.rename(columns=column_mapping)
     data_list = df.to_dict(orient='records')  # Convert dataframe to list of dictionaries
 
     # Convert NaN values to None after converting to dictionary - AGAIN!
-    for record in data_list:
-        for key, value in record.items():
-            if isinstance(value, float) and math.isnan(value):
-                record[key] = None  # Replace NaN values with None
-
+    #for record in data_list:
+    #    for key, value in record.items():
+    #        if isinstance(value, float) and math.isnan(value):
+    #            record[key] = None  # Replace NaN values with None
+            
     grist_data = [{"fields": record} for record in data_list]  # Prepare data for Grist
 
     # Endpoint URL for fetching/updating the table
     url = f'https://api.getgrist.com/api/docs/{DOC_ID}/tables/{TABLE_NAME}/records'
 
     # Get all rowIds and delete existing records
-    response = handle_response(session.get(url))  # Handle response
-    row_ids = [r["id"] for r in response.json()["records"]]  # Get row ids
-    delete_url = f'https://api.getgrist.com/api/docs/{DOC_ID}/tables/{TABLE_NAME}/data/delete'
-    response = handle_response(session.post(delete_url, json=row_ids))  # Delete existing records
+    #response = handle_response(session.get(url))  # Handle response
+    #row_ids = [r["id"] for r in response.json()["records"]]  # Get row ids
+    #delete_url = f'https://api.getgrist.com/api/docs/{DOC_ID}/tables/{TABLE_NAME}/data/delete'
+    #response = handle_response(session.post(delete_url, json=row_ids))  # Delete existing records
 
     # Validate the response
     if response.status_code != 200:
